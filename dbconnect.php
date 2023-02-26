4921efbb80ce55652b86ecb4fc70b91f8e5ed5b8
kaltr1ne <120459164+kaltr1ne@users.noreply.github.com>
2023-02-26 23:17:43 +0100

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,26 @@
+<?php
+class dbConnect{
+    private $conn = '';
+    private $hostiIDB = 'localhost';
+    private $emriDB = 'lovesac';
+    private $dbUsername = 'root';
+    private $dbPass = '';
+
+    public function connDB()
+    {
+        try {
+            $this->conn = new PDO(
+                "mysql:host=$this->hostiIDB;dbname=$this->emriDB", $this->dbUsername, $this->dbPass,
+                [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+            );
+        } catch (PDOException $pdoe) {
+            die("Nuk mund të lidhej me bazën e të dhënave {$this->emriDB} :" . $pdoe->getMessage());
+        }
+
+        return $this->conn;
+    }
+
+
+}
+
+?>
