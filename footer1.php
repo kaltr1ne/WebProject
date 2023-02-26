a200aa07a33e86af431c6c48681af4ddbbf5c775
kaltr1ne <120459164+kaltr1ne@users.noreply.github.com>
2023-02-26 23:08:25 +0100

    Add files via upload

---
+++ footer1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lovesac</title>
+    <link rel="stylesheet" href="../footer.css">
+    <link rel="shortcut icon" type="x-icon" href="img/logo/logo_200x200.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="https://fontawesome.com/icons/circle-xmark?s=solid&f=classic">
+    <link rel="stylesheet" href="https://fontawesome.com/icons/arrow-right?s=light&f=classic">
+    <script src="https://kit.fontawesome.com/3880c5a2c3.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    
+    <footer class="section-p1">
+        <div class="col">
+            <img class="logo" src="logo_200x200.png" alt="">
+            <h4>Contact</h4>
+            <p><strong>Street: </strong>566 Gonzales St. South Plainfield, NJ 07080</p>
+            <p><strong>Phone: </strong>559-281-1263</p>
+            <p><strong>E-mail: </strong>lovesac@gmail.com</p>
+            <div class="follow">
+                <h4>Follow us</h4>
+                <div></div>
+                <a href="https://facebook.com"><i class="fab fa-facebook-f"></a></i>
+                    <a href="https://twitter.com"><i class="fab fa-twitter"></a></i>
+                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></a></i>
+                    <a href="https://www.pinterest.com/"><i class="fab fa-pinterest-p"></a></i>
+                    <a href="https://www.youtube.com/"><i class="fab fa-youtube"></a></i>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <h4>About</h4>
+            <a onclick="window.location.href='about.php'" href="#">About Us</a>
+            <a href="#">Delivery Information</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a onclick="window.location.href='contact.php'"href="#">Contact Us</a>
+        </div>
+        <div class="col">
+            <h4>My Account</h4>
+            <a onclick="window.location.href='signup.php'"href="#">Sign In</a>
+            <a onclick="window.location.href='index.php'"href="#">View In</a>
+            <a href="#">My Wishlist</a>
+            <a onclick="window.location.href='contact.php'"href="#">Help</a>
+        </div>
+    </footer>
+    
+
+    <script src="script.js"></script>
+</body>
+</html>
