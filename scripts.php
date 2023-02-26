97406b08d9faaeb64a81a221e0956ea0e437e811
kaltr1ne <120459164+kaltr1ne@users.noreply.github.com>
2023-02-26 23:14:37 +0100

    Add files via upload

---
+++ scripts.php
@@ -0,0 +1,7 @@
+<?php
+echo 
+/*<script src="../../js/slider.js"></script>*/
+/*<script src="../../js/hamburgerMenu.js"></script>*/
+'<script src="script1.js"></script>
+<script src="closeMessage.js"></script>
+'?>
