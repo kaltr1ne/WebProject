ce4427a8dff1b70cb9f2f5282579ecb9908dfedd
kaltr1ne <120459164+kaltr1ne@users.noreply.github.com>
2023-02-26 23:14:03 +0100

    Add files via upload

---
+++ error/404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Lovesac</title>
+  <link rel="shortcut icon" href="../../img/web/favicon.ico" />
+  <link rel="stylesheet" href="rror/404.css" />
+</head>
+
+<body>
+  <?php include 'header.php' ?>
+
+  <div class="about">
+    <h3 class="title">403 | Nuk keni akses per tu qasur ne kete faqe!</h3>
+  </div>
+
+  <?php include 'footer.php' ?>
+</body>
+
+</html>
